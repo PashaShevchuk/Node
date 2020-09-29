5daa716c40412289bd0ff4f6beb75a41a42a4997
PashaShevchuk <pasha608@gmail.com>
2020-09-29 19:31:22 +0300

    node cron-jobs, cors

--- lesson_10_node_cron_jobs_cors/app.js
+++ lesson_10_node_cron_jobs_cors/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
 const sequelize = require('./dataBase');
